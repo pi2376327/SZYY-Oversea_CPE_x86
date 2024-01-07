5c0e86ead6c469799c6532faeea718864657ba61
pi2376327 <82651004+pi2376327@users.noreply.github.com>
2024-01-07 14:35:36 +0800

    Update openvpn-watchdog.sh

--- files/root/script/openvpn-watchdog.sh
+++ files/root/script/openvpn-watchdog.sh
@@ -1,27 +1,41 @@
 #!/bin/sh
 
 DATE=`date +%Y-%m-%d-%H:%M:%S`
-tries=0
-echo  $DATE: Start to check gateway of vpn-tun0
-echo  $DATE: Start to check gateway of vpn-tun0 >>openvpn_watchdog.log
-while [[ $tries -lt 5 ]]
-do
-        if /bin/ping -c 1 172.30.0.1 >/dev/null
+
+function exist_tun0(){
+       ip addr show tun0 > /dev/null 2>&1
+       return $?
+}
+
+if ! exist_tun0;then
+    echo tun0 not exist,restart openvpn >>/root/script/openvpn_watchdog.log
+    /etc/init.d/openvpn restart
+    exit 0
+else             
+    #check gateway of vpn
+    tries=0
+    gw=$(ip addr show tun0 | grep 'inet ' | cut -d' ' -f6 | cut -d'.' -f1-3).1
+    echo  $DATE: Start to check gateway of vpn-tun0
+    echo  $DATE: Start to check gateway of vpn-tun0 >>/root/script/openvpn_watchdog.log
+    while [[ $tries -lt 5 ]]
+    do
+        if /bin/ping -c 1 $gw >/dev/null
         then
                 echo $DATE: Gateway is OK
                 echo $DATE: Exit
-                echo $DATE: Gateway is OK >> openvpn_watchdog.log
+                echo $DATE: Gateway is OK >> /root/script/openvpn_watchdog.log
                 exit 0
         fi
         tries=$((tries+1))
         sleep 10
-        echo $DATE tries: $tries
-        echo $DATE tries: $tries >> openvpn_watchdog.log
-done
+        echo $DATE: tries: $tries
+        echo $DATE: tries: $tries >> /root/script/openvpn_watchdog.log
+    done
 
-echo $DATE openvpn restart
-echo $DATE openvpn restart >> openvpn_watchdog.log
-/etc/init.d/openvpn restart
+    echo $DATE: openvpn restart
+    echo $DATE: openvpn restart >> /root/script/openvpn_watchdog.log
+    /etc/init.d/openvpn restart
 
-#echo $DATE reboot >> openvpn_watchdog.log
-#reboot
+    #echo $DATE reboot >> /root/script/openvpn_watchdog.log
+    #reboot
+fi
