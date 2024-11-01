7e85077780fc20e79beed8e42c3f2e9340155c7a
pi2376327 <82651004+pi2376327@users.noreply.github.com>
2024-11-01 17:16:57 +0800

    Create openvpn-down.sh

---
+++ files/etc/openvpn/openvpn-down.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+iptables -t mangle -D PREROUTING -m set ! --match-set chnroute dst -j MARK --set-mark 1
+iptables -t mangle -D OUTPUT -m set ! --match-set chnroute dst -j MARK --set-mark 1
+ip route del default table 100
+ip rule del from all fwmark 1 table 100
