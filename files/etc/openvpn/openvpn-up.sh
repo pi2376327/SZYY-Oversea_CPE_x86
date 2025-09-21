dd79144eb016f8dfa8c279d553fb401a126a6018
pi2376327 <82651004+pi2376327@users.noreply.github.com>
2025-09-21 17:24:15 +0800

    Update openvpn-up.sh

--- files/etc/openvpn/openvpn-up.sh
+++ files/etc/openvpn/openvpn-up.sh
@@ -2,7 +2,7 @@
 
 #add route-policy
 src=$(ip addr show tun0 | grep 'inet ' | sed 's/^.*inet //g' | sed 's/\/16.*$//g')
-gw=$(ip addr show tun0 | grep 'inet ' | cut -d' ' -f6 | cut -d'.' -f1-3).1
+gw=$(ip addr show tun0 | grep 'inet ' | cut -d' ' -f6 | cut -d'.' -f1-2).0.1
 ip route add default via $gw dev tun0 src $src table 100 
 ip rule add from all fwmark 1 table 100
 #ip route add 8.8.8.8 via $gw dev tun0 src $src
