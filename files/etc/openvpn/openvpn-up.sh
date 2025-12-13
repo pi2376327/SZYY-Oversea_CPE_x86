15c6782014f7a453782ca94e945cb801471cfe7a
pi2376327 <82651004+pi2376327@users.noreply.github.com>
2025-12-13 17:36:09 +0800

    Log OpenVPN client startup message

--- files/etc/openvpn/openvpn-up.sh
+++ files/etc/openvpn/openvpn-up.sh
@@ -63,4 +63,4 @@ else
         echo "$DATE: The $ips is a private ip address" >>/root/script/ovpn-script.log
 fi
 
-echo "$DATE: Openvpn-client startup"
+echo "$DATE: Openvpn-client startup" >>/root/script/ovpn-script.log
