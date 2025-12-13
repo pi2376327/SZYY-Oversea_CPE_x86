27db9382bb6c41ddb807c503e7fc29d0d555a31f
pi2376327 <82651004+pi2376327@users.noreply.github.com>
2025-12-13 17:38:30 +0800

    Add logging for OpenVPN client disconnection
    Log a message when OpenVPN client disconnects.

--- files/etc/openvpn/openvpn-down.sh
+++ files/etc/openvpn/openvpn-down.sh
@@ -3,3 +3,5 @@ iptables -t mangle -D PREROUTING -m set ! --match-set chnroute dst -j MARK --set
 iptables -t mangle -D OUTPUT -m set ! --match-set chnroute dst -j MARK --set-mark 1
 ip route del default table 100
 ip rule del from all fwmark 1 table 100
+
+echo "$DATE: Openvpn-client disconnected" >>/root/script/ovpn-script.log
