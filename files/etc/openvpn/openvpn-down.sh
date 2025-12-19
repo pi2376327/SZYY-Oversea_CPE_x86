11ee6176e80575e5ee3a90fa411221dd85c94874
pi2376327 <82651004+pi2376327@users.noreply.github.com>
2025-12-19 22:19:13 +0800

    Update openvpn-down.sh

--- files/etc/openvpn/openvpn-down.sh
+++ files/etc/openvpn/openvpn-down.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
+DATE=`date +%Y-%m-%d-%H:%M:%S`
+
 iptables -t mangle -D PREROUTING -m set ! --match-set chnroute dst -j MARK --set-mark 1
 iptables -t mangle -D OUTPUT -m set ! --match-set chnroute dst -j MARK --set-mark 1
 ip route del default table 100
 ip rule del from all fwmark 1 table 100
 
+echo "----------------------------------------------------------------" >>/root/script/ovpn-script.log
 echo "$DATE: Openvpn-client disconnected" >>/root/script/ovpn-script.log
